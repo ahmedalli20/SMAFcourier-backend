f0635546e41a66898e80c031f31c97dc0c96a9ab
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-12-05 11:20:22 +0300

    added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  #  resources :users
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
