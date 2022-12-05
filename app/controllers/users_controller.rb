c636d054d8ae7cc48b93c36fdcfa4b60f07e3d67
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-12-05 11:15:37 +0300

    added methods to the user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+    def new
+        user = User.new
+      end
+    
+      
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user
+      end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+   end
+
+     private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+end
