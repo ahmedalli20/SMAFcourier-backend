9c365b99acf27f5dcf4b12ab5909857364d88395
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-12-05 11:21:03 +0300

    added the data to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,15 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "🐕 Seeding data..."
+
+User.create(username: "Luke", password:"lukasam", password_confirmation:"lukasam")
+User.create(username: "mahrez", password:"mahrezali", password_confirmation:"mahrezali")
+User.create(username: "dembele", password:"dembelee", password_confirmation:"dembelee")
+User.create(username: "dele", password:"deleali", password_confirmation:"deleali")
+User.create(username: "maahir", password:"mahrezy", password_confirmation:"mahrezy")
+
+
+puts "✅ Done seeding!"
