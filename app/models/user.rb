9f40d4ebde902784c1b7f3d46f1b936cb63ac5d8
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-12-05 11:18:11 +0300

    added validation to the user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    has_secure_password
+     validates :username, presence: true
+     validates :password, :presence => true,:length => {:within => 6..30},  :on => :create
+    # :confirmation => true,
+   
+  
+
+end
