1282271a091b64f9a45ffc074fec532bbae8ed56
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-12-05 11:14:08 +0300

    added an authentication token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    include ActionController::Cookies
+    skip_before_action :verify_authenticity_token
+  
+  
+    # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    
+    
+    #   def render_unprocessable_entity_response(e)
+    #     render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+    #   end
 end
