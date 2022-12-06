ef57180489f7d75b910e6f42b10ca737855dacd3
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-12-06 12:26:27 +0300

    added the relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
     has_secure_password
      validates :username, presence: true
      validates :password, :presence => true,:length => {:within => 6..30},  :on => :create
+
     # :confirmation => true,
    
-  
+  has_many :oders
 
 end
