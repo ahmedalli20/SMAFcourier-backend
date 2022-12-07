364a2da54c63be36408f6e01e456847f32103bd4
Sugut <sugutvictorine@gmail.com>
2022-12-07 20:04:19 +0300

    done changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ApplicationRecord
      validates :username, presence: true
      validates :password, :presence => true,:length => {:within => 6..30},  :on => :create
     # :confirmation => true,
-   
-  
 
 end
