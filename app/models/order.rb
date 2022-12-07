8231c6e78655437f020f37f21d47f001f696331a
Felix Ondullah <felixondullah@gmail.com>
2022-12-07 23:15:55 +0300

    changes done

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,13 @@
 class Order < ApplicationRecord
     # belongs_to :user
+    validates :package_type, :pick_up_location, :drop_off_location, :date, :sender_name, :number_of_kgs, :receiver_name
 end
+
+
+# t.string "package_type"
+# t.string "pick_up_location"
+# t.string "drop_off_location"
+# t.string "date"
+# t.string "sender_name"
+# t.integer "number_of_kgs"
+# t.string "receiver_name"
